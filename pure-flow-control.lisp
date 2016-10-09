96060d851542b042b4005b5d3cdae116bb6d15e5
baggers <baggers@cave>
2016-10-09 12:03:59 +0200

    add special-ops and macros (as uncategorized)

--- pure-flow-control.lisp
+++
@@ -1,9 +0,0 @@
-;;;; package containing all pure flow control forms from common lisp
-;;;; This means forms where the jumps are within the form only and
-;;;; have no implicit progn e.g. if
-
-(defpackage #:cl-pure-flow
-  (:use #:tamei-common)
-  (:import-from :cl
-                #:if)
-  (:export #:if))
